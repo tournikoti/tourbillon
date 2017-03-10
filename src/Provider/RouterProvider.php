4fd2087249e0a808d33743e78ae038f690b4d654
Gwennael Jean <gwennael.jean@trsb.net>
2017-03-10 09:18:49 +0100

    feat: creation du RouterProvider

---
+++ src/Provider/RouterProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tourbillon\Provider;
+
+use Tourbillon\Configurator\Configurator;
+use Tourbillon\ServiceContainer\ServiceProvider;
+
+/**
+ * Description of Router
+ *
+ * @author gjean
+ */
+class RouterProvider extends ServiceProvider
+{
+    
+    public function configuration(Configurator $configurator)
+    {
+
+    }
+
+}
