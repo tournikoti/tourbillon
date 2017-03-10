2c2e7c486ca72afc112c6bbfa9f57b6c96aae20b
Gwennael Jean <gwennael.jean@trsb.net>
2017-03-10 09:59:58 +0100

    feat: developpement de la methode createInstance du routerProvider

--- src/Provider/RouterProvider.php
+++ src/Provider/RouterProvider.php
@@ -3,6 +3,8 @@
 namespace Tourbillon\Provider;
 
 use Tourbillon\Configurator\Configurator;
+use Tourbillon\Request\HttpRequest;
+use Tourbillon\Router\Router;
 use Tourbillon\ServiceContainer\ServiceProvider;
 
 /**
@@ -12,10 +14,20 @@ use Tourbillon\ServiceContainer\ServiceProvider;
  */
 class RouterProvider extends ServiceProvider
 {
-    
+    private $configurator;
+    private $router;
+
     public function configuration(Configurator $configurator)
     {
+        $this->configurator = $configurator;
+    }
 
+    public function createInstance(HttpRequest $request)
+    {
+        if (null === $this->router) {
+            $this->router = new Router($request, $this->configurator->get('routing'));
+        }
+        return $this->router;
     }
 
 }
