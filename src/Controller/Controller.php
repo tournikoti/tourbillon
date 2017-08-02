0c4080c060655855d41a6eb8e0663d0dbc0c524c
Jean Gwennael <gwennael.jean@trsb.net>
2017-08-02 09:34:25 +0200

    chore: ajout des commentaires sur les methode render et getConfig

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -18,12 +18,22 @@ abstract class Controller
         $this->serviceLocator = $serviceLocator;
     }
 
+    /**
+     * 
+     * @param type $path
+     * @param array $params
+     * @return \Tourbillon\Response\View
+     */
     protected function render($path, array $params = array())
     {
         $viewConfig = $this->getConfig()->get('view');
         return $this->serviceLocator->get('view.factory')->create($viewConfig['resources'] . '/' . $path, $params, $viewConfig['template']);
     }
 
+    /**
+     * Retourne l'instance de Configurator
+     * @return \Tourbillon\Configurator\Configurator
+     */
     protected function getConfig()
     {
         return $this->serviceLocator->get('config');
